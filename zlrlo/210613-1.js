b7838ed851ec380c52171ac86c6aa8d6b8371dfa
zlrlo <easy10612@gmail.com>
2021-06-13 16:12:04 +0900

    leetcode20: Valid Parentheses

---
+++ zlrlo/210613-1.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+    let stack =[]; 
+    let answer = true; 
+    
+    s.split('').forEach((ch, index) => {
+        if(ch === '(' || ch === '[' || ch === '{') stack.push(ch);
+        else {
+            let pre = stack.pop();
+            
+            if(pre) {
+                if(ch === ')' && pre !== '(') answer = false;
+                if(ch === ']' && pre !== '[') answer = false; 
+                if(ch === '}' && pre !== '{') answer = false; 
+            }
+            else answer = false; 
+        }
+    }); 
+    
+    if(stack.length !== 0) answer = false; 
+    return answer;
+};
